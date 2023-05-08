36752d60e084477e725c3db714a3a007e4079576
Tamerlan Aliyev <tamerlan-aliyev-2021@mail.ru>
2023-05-08 20:50:23 +0400

    change allowed host to allowed hosts

--- Searchart/settings.py
+++ Searchart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1^^thduel)ra7ey10dsmf4=#!vdsf5o*j%itu03ti&%yef6))+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
