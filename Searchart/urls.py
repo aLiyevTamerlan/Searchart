258e30fe8a5fa8a40bfc7882ce624f3e9b3c42e2
Tamerlan Aliyev <tamerlan-aliyev-2021@mail.ru>
2023-05-08 21:06:11 +0400

    add staticfiles root

--- Searchart/urls.py
+++ Searchart/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from app.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', home),
     path('api-auth/', include('rest_framework.urls'))
 ]
-
+urlpatterns = []+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
