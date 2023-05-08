18cccdf2505db046067eb3dc908139d570d343e1
Tamerlan Aliyev <tamerlan-aliyev-2021@mail.ru>
2023-05-08 17:36:05 +0400

    add template

--- Searchart/urls.py
+++ Searchart/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from app.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls.api_urls')),
+    path('', home),
     path('api-auth/', include('rest_framework.urls'))
 ]
 
